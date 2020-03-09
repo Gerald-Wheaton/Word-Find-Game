0bdee86d9de02d09bad7b6dbc265a8b26af66e6f
Gerald-Wheaton <geraldwheaton2000@gmail.com>
2020-03-09 11:40:02 -0400

    added diagonal compas functions

--- WordFind.java
+++ WordFind.java
@@ -81,24 +81,45 @@ public class WordFind {
                 if (wordGrid[rowIndex][columnIndex] == word.charAt(0)) {
 
                     if (North(word, wordGrid, rowIndex, columnIndex) == true) {
-                        System.out.println(word + " was found starting at (" + rowIndex + ", " + columnIndex + ") and oriented North");
+                        System.out.println(word + " was found starting at (" + (rowIndex + 1) + ", " + (columnIndex + 1) + ") and oriented North");
                         return true;
                     }
 
                     if (South(word, wordGrid, rowIndex, columnIndex, colLength) == true) {
-                        System.out.println(word + " was found starting at (" + rowIndex + ", " + columnIndex + ") and oriented South");
+                        System.out.println(word + " was found starting at (" + (rowIndex + 1) + ", " + (columnIndex + 1) + ") and oriented South");
                         return true;
                     }
 
                     if (West(word, wordGrid, rowIndex, columnIndex) == true) {
-                        System.out.println(word + " was found starting at (" + rowIndex + ", " + columnIndex + ") and oriented West");
+                        System.out.println(word + " was found starting at (" + (rowIndex + 1) + ", " + (columnIndex + 1) + ") and oriented West");
                         return true;
                     }
 
                     if (East(word, wordGrid, rowIndex, columnIndex, rowLength) == true) {
-                        System.out.println(word + " was found starting at (" + rowIndex + ", " + columnIndex + ") and oriented East");
+                        System.out.println(word + " was found starting at (" + (rowIndex + 1) + ", " + (columnIndex + 1) + ") and oriented East");
                         return true;
                     }
+
+                    if (NorthEast(word, wordGrid, rowIndex, columnIndex, rowLength) == true) {
+                        System.out.println(word + " was found starting at (" + (rowIndex + 1) + ", " + (columnIndex + 1) + ") and oriented Northeast");
+                        return true;
+                    }
+
+                    if (NorthWest(word, wordGrid, rowIndex, columnIndex) == true) {
+                        System.out.println(word + " was found starting at (" + (rowIndex + 1) + ", " + (columnIndex + 1) + ") and oriented Northwest");
+                        return true;
+                    }
+
+                    if (SouthWest(word, wordGrid, rowIndex, columnIndex, colLength) == true) {
+                        System.out.println(word + " was found starting at (" + (rowIndex + 1) + ", " + (columnIndex + 1) + ") and oriented Southwest");
+                        return true;
+                    }
+
+                    if (SouthEast(word, wordGrid, rowIndex, columnIndex, colLength, rowLength) == true) {
+                        System.out.println(word + " was found starting at (" + (rowIndex + 1) + ", " + (columnIndex + 1) + ") and oriented Southeast");
+                        return true;
+                    }
+
                 }
             }
         }
@@ -118,6 +139,33 @@ public class WordFind {
         return true;
     }
 
+    public static boolean NorthEast(String word, char[][] wordGrid, int rowIndex, int columnIndex, int rowLength) {
+        if (columnIndex + word.length() > rowLength || rowIndex < word.length() - 1) {
+            return false;
+        }
+        for (int i = 0; i < word.length(); i++) {
+            //if current character in the grid != currnet character in the word exit the loop
+            if (wordGrid[rowIndex - i][columnIndex + i] != word.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean NorthWest(String word, char[][] wordGrid, int rowIndex, int columnIndex) {
+        if (columnIndex < word.length() - 1 || rowIndex < word.length() - 1) {
+            return false;
+        }
+        for (int i = 0; i < word.length(); i++) {
+            //if current character in the grid != currnet character in the word exit the loop
+            if (wordGrid[rowIndex - i][columnIndex - i] != word.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
     public static boolean South(String word, char[][] wordGrid, int rowIndex, int columnIndex, int colLength) {
         if (rowIndex + word.length() > colLength) {
             return false;
@@ -130,6 +178,33 @@ public class WordFind {
         }
         return true;
     }
+
+    public static boolean SouthWest(String word, char[][] wordGrid, int rowIndex, int columnIndex, int colLength) {
+        if (columnIndex < word.length() - 1 || rowIndex + word.length() > colLength) {
+            return false;
+        }
+        for (int i = 0; i < word.length(); i++) {
+            //if current character in the grid != currnet character in the word exit the loop
+            if (wordGrid[rowIndex + i][columnIndex - i] != word.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean SouthEast(String word, char[][] wordGrid, int rowIndex, int columnIndex, int colLength, int rowLength) {
+        if (columnIndex + word.length() > rowLength || rowIndex + word.length() > colLength) {
+            return false;
+        }
+        for (int i = 0; i < word.length(); i++) {
+            //if current character in the grid != currnet character in the word exit the loop
+            if (wordGrid[rowIndex + i][columnIndex + i] != word.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
     public static boolean West(String word, char[][] wordGrid, int rowIndex, int columnIndex) {
         if (columnIndex < word.length() - 1) {
@@ -157,6 +232,7 @@ public class WordFind {
         return true;
     }
 
-    
+
+
 
 }
